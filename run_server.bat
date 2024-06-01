5469dd686cf7fb4e32ed2e678f730b6e18ad34fa
EDINSON <cuentapersonal145@gmail.com>
2024-06-01 18:13:11 -0500

    second commit

---
+++ run_server.bat
@@ -0,0 +1,6 @@
+@echo off
+cd D:\MIS DOCUMENTOS\LENOVO\Desktop\Tecnologias\Django\env\Scripts
+call activate.bat
+cd D:\MIS DOCUMENTOS\LENOVO\Desktop\Trabajo de grado\Pagina web\BackEnd Django\Gestion_Proyectos_FIT
+python manage.py runserver
+exit
---
+++ run_web.bat
@@ -0,0 +1,5 @@
+@echo off
+cd D:\MIS DOCUMENTOS\LENOVO\Desktop\Trabajo de grado\Pagina web\BackEnd Django
+START run.bat
+START chrome.exe http://localhost:8000/admin
+exit
