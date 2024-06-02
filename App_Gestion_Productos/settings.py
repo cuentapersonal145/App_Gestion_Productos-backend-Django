a3ea36900145a7dbc0e7d31cc60cbb1fad2bf911
EDINSON <cuentapersonal145@gmail.com>
2024-06-01 19:18:33 -0500

    ubicacion de bd y renombre de .bat

--- App_Gestion_Productos/settings.py
+++ App_Gestion_Productos/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'App_Gestion_Productos.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database/db.sqlite3',
     }
 }
 
