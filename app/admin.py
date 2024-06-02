a05d92cd68c8cd467715ed8395e11b54bdeced1d
EDINSON <cuentapersonal145@gmail.com>
2024-06-02 13:26:48 -0500

    nueva base de datos

--- app/admin.py
+++ app/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import *
 
 class ItemAdmin(admin.ModelAdmin):
-    search_fields = ('detalle','codigo_barras',)
+    search_fields = ('descripcion','codigo_barras',)
 
 admin.site.register([
     Articulo
--- app/models.py
+++ app/models.py
@@ -3,18 +3,20 @@ from django.db import models
 class Articulo(models.Model):
     id = models.AutoField(primary_key=True)
     codigo_barras = models.CharField(max_length=64, null=False, blank=False)
-    detalle = models.CharField(max_length=255, null=False, blank=False)
-    precio = models.PositiveIntegerField()
+    descripcion = models.CharField(max_length=255, null=False, blank=False)
+    precio_compra = models.PositiveIntegerField()
+    precio_venta = models.PositiveIntegerField()
+    existencia = models.PositiveIntegerField()
     
     date_record = models.DateTimeField(auto_now_add=True)
     date_update = models.DateTimeField(auto_now=True) 
     is_active = models.BooleanField(default=True)
 
     class Meta:
-        ordering = ['detalle']
+        ordering = ['descripcion']
         verbose_name = ("Articulo")
         verbose_name_plural = ("Articulos")
         db_table = 'ARTICULO'
         
     def __str__(self):
-        return self.detalle + ' : $' + str(self.precio) + '. [CD:' + self.codigo_barras + '] (ID:' + str(self.id) + ')'
+        return self.descripcion + ' : $' + str(self.precio_venta) + '. [CD:' + self.codigo_barras + '] (ID:' + str(self.id) + ')'
