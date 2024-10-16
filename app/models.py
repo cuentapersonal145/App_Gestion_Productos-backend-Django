475aa7c1c2f731404045ef9d2eca1e23d36a00ef
EDINSON <cuentapersonal145@gmail.com>
2024-10-16 16:54:15 -0500

    Correccion en modelo/formulario para el registro de un articulo

--- app/models.py
+++ app/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Articulo(models.Model):
     id = models.AutoField(primary_key=True)
-    codigo_barras = models.CharField(max_length=64, null=False, blank=False)
+    codigo_barras = models.CharField(max_length=64, null=False, blank=True, default="0")
     descripcion = models.CharField(max_length=255, null=False, blank=False)
-    precio_compra = models.PositiveIntegerField()
-    precio_venta = models.PositiveIntegerField()
-    existencia = models.PositiveIntegerField()
+    precio_compra = models.PositiveIntegerField(null=True, blank=True, default=0)
+    precio_venta = models.PositiveIntegerField(null=False, blank=False)
+    existencia = models.PositiveIntegerField(null=True, blank=True, default=0)
     
     date_record = models.DateTimeField(auto_now_add=True)
     date_update = models.DateTimeField(auto_now=True) 
